293f3734a2614e59d8df6cbdb2bb27b2952ed16e
武漢大学で一番生徒いかずちともします <leibohan@126.com>
2019-01-17 21:58:12 +0800

    2019-01-17 update

--- src/storage/Class/Memory.java
+++ src/storage/Class/Memory.java
@@ -1,5 +1,19 @@
 package storage.Class;
 
 public class Memory {
-
+	MainStorage[] content = new MainStorage[8192];
+	boolean [][] used_map = new boolean [8192][1024];
+	
+	public Memory() {
+		for (int i = 0; i < 8192; i++) 
+			for (int j = 0; j < 1024; j++) 
+				used_map[i][j] = false;
+	}
+	
+	/*
+	public void main(String[] args) {
+		boolean [] a = used_map[3000];
+		System.out.print(a[0]);
+	}
+	*/
 }
